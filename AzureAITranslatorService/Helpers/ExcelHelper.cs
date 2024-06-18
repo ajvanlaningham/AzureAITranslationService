fe1ce877b2e79ce2abca5b17dc638641b4ea79c4
Alex VanLaningham <ajvanlaningham@gmail.com>
2024-06-18 18:51:46 -0400

    updated Excel helper,new TranlationEntry model

--- AzureAITranslatorService/Helpers/ExcelHelper.cs
+++ AzureAITranslatorService/Helpers/ExcelHelper.cs
@@ -2,10 +2,13 @@
 using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 
 public static class ExcelHelper
 {
+    private static readonly TraceSource traceSource = new TraceSource("ExcelHelperTraceSource");
+
     static ExcelHelper()
     {
         ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
@@ -15,15 +18,39 @@ public static class ExcelHelper
     {
         string excelFilePath = Path.ChangeExtension(resxFilePath, ".xlsx");
 
+        if (File.Exists(excelFilePath))
+        {
+            // Log a message that the file already exists
+            traceSource.TraceEvent(TraceEventType.Information, 0, $"File already exists: {excelFilePath}");
+
+            // Optionally, handle the file appropriately, e.g., delete, rename, or append
+            File.Delete(excelFilePath);
+            traceSource.TraceEvent(TraceEventType.Information, 0, $"Deleted existing file: {excelFilePath}");
+        }
+
         using (ExcelPackage package = new ExcelPackage())
         {
-            // Initially, we just create the file without adding any data
+            // Create the worksheet
             ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Resources");
 
+            // Add headers
+            worksheet.Cells[1, 1].Value = "Key";
+            worksheet.Cells[1, 2].Value = "Value";
+            worksheet.Cells[1, 3].Value = "Comment";
+
+            // Add data
+            for (int i = 0; i < resxEntries.Count; i++)
+            {
+                worksheet.Cells[i + 2, 1].Value = resxEntries[i].Name;
+                worksheet.Cells[i + 2, 2].Value = resxEntries[i].Value;
+                worksheet.Cells[i + 2, 3].Value = resxEntries[i].Comment;
+            }
+
+            // Save the file
             FileInfo fileInfo = new FileInfo(excelFilePath);
             package.SaveAs(fileInfo);
         }
 
-        Console.WriteLine($"Excel file created: {excelFilePath}");
+        traceSource.TraceEvent(TraceEventType.Information, 0, $"Excel file created: {excelFilePath}");
     }
 }
---
+++ AzureAITranslatorService/Models/TranslationEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureAITranslatorService.Models
+{
+    public class TranslationEntry
+    {
+        public string Name { get; set; }
+        public string SourceLanguage { get; set; }
+        public string TargetLanguage { get; set; }
+        public string Comment { get; set; }
+    }
+}
