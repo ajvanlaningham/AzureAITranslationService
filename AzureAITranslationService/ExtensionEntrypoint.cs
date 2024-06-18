75cc8e6d66f73fd4750dee71e4119f98f50c657b
Alex VanLaningham <ajvanlaningham@gmail.com>
2024-06-18 11:08:41 -0400

    initial commit

---
+++ AzureAITranslationService/ExtensionEntrypoint.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Extensibility;
+
+namespace AzureAITranslationService
+{
+    /// <summary>
+    /// Extension entrypoint for the VisualStudio.Extensibility extension.
+    /// </summary>
+    [VisualStudioContribution]
+    internal class ExtensionEntrypoint : Extension
+    {
+        /// <inheritdoc/>
+        public override ExtensionConfiguration ExtensionConfiguration => new()
+        {
+            Metadata = new(
+                    id: "AzureAITranslationService.03784632-69f8-4bdb-a54c-9e434e050a92",
+                    version: this.ExtensionAssemblyVersion,
+                    publisherName: "Publisher name",
+                    displayName: "AzureAITranslationService",
+                    description: "Extension description"),
+        };
+
+        /// <inheritdoc />
+        protected override void InitializeServices(IServiceCollection serviceCollection)
+        {
+            base.InitializeServices(serviceCollection);
+
+            // You can configure dependency injection here by adding services to the serviceCollection.
+        }
+    }
+}
