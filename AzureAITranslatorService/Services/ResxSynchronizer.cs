f12d31dd8e6e69b4351affed1008ae668277eb15
Alex VanLaningham <ajvanlaningham@gmail.com>
2024-06-23 20:14:50 -0400

    hotfix, updated sync logic, resources bundled for export

--- AzureAITranslatorService/Services/ResxSynchronizer.cs
+++ AzureAITranslatorService/Services/ResxSynchronizer.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Resources;
 using System.Xml.Linq;
 
 public static class ResxSynchronizer
@@ -37,22 +34,21 @@ public static class ResxSynchronizer
             var sourceEntries = ReadResxFile(sourceResxPath);
             var targetEntries = ReadResxFile(targetResxPath);
 
-            var allKeys = new HashSet<string>(sourceEntries.Keys.Concat(targetEntries.Keys));
+            var updatedTargetEntries = new OrderedDictionary();
 
-            var updatedTargetEntries = new Dictionary<string, (string Value, string Comment)>();
-            foreach (var key in allKeys)
+            // Add existing target entries first
+            foreach (DictionaryEntry entry in targetEntries)
             {
-                if (targetEntries.ContainsKey(key))
+                updatedTargetEntries[entry.Key] = entry.Value;
+            }
+
+            // Add new entries from source
+            foreach (DictionaryEntry entry in sourceEntries)
+            {
+                if (!targetEntries.Contains(entry.Key))
                 {
-                    updatedTargetEntries[key] = targetEntries[key];
-                }
-                else if (sourceEntries.ContainsKey(key))
-                {
-                    updatedTargetEntries[key] = sourceEntries[key];
-                }
-                else
-                {
-                    updatedTargetEntries[key] = (string.Empty, string.Empty); // Add empty value and comment for missing keys
+                    var valueTuple = ((string Value, string Comment))entry.Value;
+                    updatedTargetEntries[entry.Key] = (valueTuple.Value, "New");
                 }
             }
 
@@ -64,9 +60,9 @@ public static class ResxSynchronizer
         }
     }
 
-    private static Dictionary<string, (string Value, string Comment)> ReadResxFile(string resxPath)
+    private static OrderedDictionary ReadResxFile(string resxPath)
     {
-        var entries = new Dictionary<string, (string Value, string Comment)>();
+        var entries = new OrderedDictionary();
         var xdoc = XDocument.Load(resxPath);
 
         foreach (var data in xdoc.Root.Elements("data"))
@@ -83,7 +79,7 @@ public static class ResxSynchronizer
         return entries;
     }
 
-    private static void WriteResxFile(string resxPath, Dictionary<string, (string Value, string Comment)> entries)
+    private static void WriteResxFile(string resxPath, OrderedDictionary entries)
     {
         var doc = new XDocument(
             new XElement("root",
@@ -102,17 +98,17 @@ public static class ResxSynchronizer
                     new XElement("value", "System.Resources.ResXResourceWriter, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"))
             )
         );
-
-        foreach (var entry in entries)
+        
+        foreach (DictionaryEntry entry in entries)
         {
             var dataElement = new XElement("data",
-                new XAttribute("name", entry.Key),
+                new XAttribute("name", (string)entry.Key),
                 new XAttribute(XNamespace.Xml + "space", "preserve"),
-                new XElement("value", entry.Value.Value));
+                new XElement("value", ((ValueTuple<string, string>)entry.Value).Item1));
 
-            if (!string.IsNullOrEmpty(entry.Value.Comment))
+            if (!string.IsNullOrEmpty(((ValueTuple<string, string>)entry.Value).Item2))
             {
-                dataElement.Add(new XElement("comment", entry.Value.Comment));
+                dataElement.Add(new XElement("comment", ((ValueTuple<string, string>)entry.Value).Item2));
             }
 
             doc.Root.Add(dataElement);
