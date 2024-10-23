26ff8a7ee2e414f708eccbc7452e283127b2bcf0
AVanLaningham <ajvanlaningham@gmail.com>
2024-10-23 19:59:42 -0400

    Added secrets service and interface, appsettings, .gitignore update

---
+++ AzureAITranslatorService/Services/ISecretsService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureAITranslatorService.Services
+{
+    public interface ISecretsService
+    {
+        string TranslatorEndpoint { get; }
+        string TranslatorKey { get; }
+        string TranslatorRegion { get; }
+    }
+}
